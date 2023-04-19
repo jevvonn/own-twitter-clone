4649865d60f4764bb2eb46294dc77ded425dba5c
jevvonn <darmawanb985@gmail.com>
2023-04-19 23:09:13 +0700

    adding post trpc router

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany();
+  }),
+});
